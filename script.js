16ce6ffadf88f6f0abf678f002d7efceda175b7d
David Shahbazyan Mushegh <david.shahbazyan.m@digitain.com>
2023-06-23 20:51:32 +0400

    first

---
+++ script.js
@@ -0,0 +1,49 @@
+const btns = document.querySelectorAll(".tab_btns-item"); 
+const contents = document.querySelectorAll(".tab_content-item"); 
+const plus = document.querySelector(".plus");
+const input = document.querySelector(".script");
+const text = document.getElementById("script");
+const btn = document.querySelector(".creat_btn");
+const myText = document.querySelectorAll("p");
+const copy = document.querySelectorAll(".copyy");
+ 
+function tabInit(initial, buttons, contents, contentClassName, buttonsClassName) { 
+    contents[initial].classList.add(contentClassName); 
+    buttons[initial].classList.add(buttonsClassName); 
+ 
+    for (let i = 0; i < buttons.length; i++) { 
+ 
+        buttons[i].addEventListener("click", function () { 
+            for (let x = 0; x < buttons.length; x++) { 
+                buttons[x].classList.remove(buttonsClassName); 
+                contents[x].classList.remove(contentClassName); 
+            } 
+            buttons[i].classList.add(buttonsClassName); 
+            contents[i].classList.add(contentClassName); 
+        }); 
+    } 
+} 
+ 
+tabInit(0, btns, contents, "active_tab", "active_btn");
+
+plus.addEventListener("click", () => {
+  text.style.display = "block"; 
+  btn.style.display = "block"; 
+});
+
+const copyButton = document.querySelector(".copy_btn");
+const text_content = document.querySelector(".text_content");
+
+copyButton.addEventListener("click", function() {
+  const text = text_content.innerText;
+
+  navigator.clipboard.writeText(text)
+    .then(function() {
+      alert("Text copied successfully!");
+    })
+    .catch(function(err) {
+      console.error("Unable to copy text:", err);
+    });
+});
+
+console.log(text_content);
